520b4e1f73edd71a636b784d28f9170e5f08f9bc
Ольга Жукова <zhukova.o.m@yandex.ru>
2025-10-17 14:03:52 +0500

    Исправила конфигурацию, чтобы не было ошибки на главной странице без слеша в конце

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/logoped-starceva/', // Замените на название вашего репозитория
+  base: '/logoped-starceva',
   build: {
     outDir: 'dist',
     sourcemap: false
   },
   server: {
-    historyApiFallback: true,
     open: true
   },
   preview: {
-    historyApiFallback: true
+    open: true
   }
 })

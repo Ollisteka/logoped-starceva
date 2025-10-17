4c229e242c5e83bd380ea50f3b7bf0796a29e190
Ольга Жукова <zhukova.o.m@yandex.ru>
2025-10-17 17:16:10 +0500

    Попробую поменять ямл конфиг

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/logoped-starceva',
+  base: '/logoped-starceva/',
   build: {
     outDir: 'dist',
     sourcemap: false
